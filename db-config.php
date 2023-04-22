57349a2974a3b2cfe477cb033ba53c9d4af6c289
Aron Paul Gonzales <aronpaulgonzales1215@gmail.com>
2023-04-22 13:15:12 +0800

    Create db-config.php

---
+++ db-config.php
@@ -0,0 +1,19 @@
+<?php
+  function connection(){
+    $host = ""; // put localhost for scratch
+    $username = ""; // username of server used
+    $password = ""; // password of server used
+    $database = ""; // name of database
+  
+
+    $con = new mysqli($host, $username, $password, $database);
+
+    if($con->connect_error){
+      echo $con->connect_error;
+    }
+    else{
+      return $con;
+    }
+
+  }
+?>
